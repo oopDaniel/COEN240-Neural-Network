7f165b0fa7e9a99a455e354114877ad6b35f9b04
Daniel <oopdaniel@gmail.com>
2019-06-12 07:26:31 -0700

    use some built-in functions

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.utils import to_categorical
+from keras import optimizers
 import numpy as np
-from tensorflow.keras.datasets import mnist
+from keras.datasets import mnist
 import sklearn.metrics as metrics
 
 # fix random seed for reproducibility
@@ -19,19 +21,17 @@ train_data, test_data = train_data / 255.0, test_data / 255.0
 hidden_layer_nodes_count = 512
 output_nodes_count = 10
 
-
-labels, test_labels = np.zeros((train_label.size, output_nodes_count)), np.zeros((test_label.size, output_nodes_count))
-for index, value in enumerate(train_label):
-    labels[index][value] = 1
-for index, value in enumerate(test_label):
-    test_labels[index][value] = 1
+# Reshape label matrices
+labels, test_labels = to_categorical(train_label), to_categorical(test_label)
 
 # Create model
 model = Sequential()
 model.add(Dense(train_data.shape[0], input_dim=train_data.shape[1], use_bias=False))
 model.add(Dense(hidden_layer_nodes_count, activation='relu'))
 model.add(Dense(output_nodes_count, activation='softmax'))
+
 # Compile model
+adam = optimizers.Adam(lr=1e-5, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 # Fit the model
 model.fit(train_data, labels, epochs=5)
